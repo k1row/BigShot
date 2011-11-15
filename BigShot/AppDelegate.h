86811a48ecde378fd55d304ce4bda410d32617ca
Keiichiro Nagashima <k.nagahsima@cyberagentamerica.com>
2011-11-15 11:51:54 -0800

    Initial Commit

---
+++ BigShot/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  BigShot
+//
+//  Created by Keiichiro Nagashima on 11/11/15.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ BigShot/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BigShot
+//
+//  Created by Keiichiro Nagashima on 11/11/15.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
